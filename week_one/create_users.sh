b253476b7130a93378e47f7a6a3be76fa8b9a028
dnetcloud <solubiyi845@stu.ui.edu.ng>
2025-05-17 09:48:45 +0100

    updating new files

--- week_one/create_users.sh
+++ week_one/create_users.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sys_admin_file = "sys-admin.csv"
+sys_admin_file="sys-admin.csv"
 PASSWORD="Password123"
+ 
 
-
-groupadd sys-admin 2>/dev/null 
+groupadd sys-admin >/dev/null 
 
 while IFS= read -r USER; do
     userdel "$USER"
-    # echo "$USER:$PASSWORD" | chpasswd
-    # usermod -aG sys-admin "$USER"
-    # echo "$USER has been added to group sys-admin"
+    echo "$USER:$PASSWORD" | chpasswd
+    usermod -aG sys-admin "$USER"
+    echo "$USER has been added to group sys-admin"
 done < "$sys_admin_file"
